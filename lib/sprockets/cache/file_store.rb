5e75163a16c9c5c1f1ab02298d8bb39fdef5f22d
Jake Mauer <jake@jakemauer.com>
2014-05-09 00:37:19 -0400

    Patches file_store.rb with atomic writing from this branch: https://github.com/rubemz/sprockets/tree/fix/atomic-write
    This patch has been applied to v2.11.0 because as of today's date, sass-rails won't work with anything newer than 2.11.0.
    
    Discussion: https://github.com/sstephenson/sprockets/pull/545

--- lib/sprockets/cache/file_store.rb
+++ lib/sprockets/cache/file_store.rb
@@ -6,27 +6,118 @@ module Sprockets
   module Cache
     # A simple file system cache store.
     #
-    #     environment.cache = Sprockets::Cache::FileStore.new("/tmp")
+    # environment.cache = Sprockets::Cache::FileStore.new("/tmp")
     #
     class FileStore
-      def initialize(root)
-        @root = Pathname.new(root)
+      DEFAULT_MAX_SIZE = 1000
+
+      def initialize(root, max_size = DEFAULT_MAX_SIZE)
+        @root = root
+        @size = find_caches.size
+        @max_size = max_size
       end
 
       # Lookup value in cache
       def [](key)
-        pathname = @root.join(key)
-        pathname.exist? ? pathname.open('rb') { |f| Marshal.load(f) } : nil
+        path = File.join(@root, "#{key}.cache")
+
+        if File.exist?(path)
+          value = File.open(path, 'rb') { |f| Marshal.load(f) }
+          FileUtils.touch(path)
+          value
+        else
+          nil
+        end
+        rescue
+          nil
       end
 
       # Save value to cache
       def []=(key, value)
-        # Ensure directory exists
-        FileUtils.mkdir_p @root.join(key).dirname
+        path = File.join(@root, "#{key}.cache")
 
-        @root.join(key).open('w') { |f| Marshal.dump(value, f)}
+        # Ensure directory exists
+        FileUtils.mkdir_p File.dirname(path)
+
+        # Check if cache exists before writing
+        exists = File.exist?(path)
+
+        # Write data
+        atomic_write(path) do |file|
+          file.write(Marshal.dump(value, file))
+        end
+
+        # GC if necessary
+        @size += 1 unless exists
+        gc! if @size > @max_size
+
         value
       end
+
+      private
+        # extracted from https://github.com/rails/rails/blob/master/activesupport/lib/active_support/core_ext/file/atomic.rb
+        def atomic_write(file_name, temp_dir = Dir.tmpdir)
+          require 'tempfile' unless defined?(Tempfile)
+          require 'fileutils' unless defined?(FileUtils)
+
+          temp_file = Tempfile.new(File.basename(file_name), temp_dir)
+          temp_file.binmode
+          yield temp_file
+          temp_file.close
+
+          if File.exist?(file_name)
+            # Get original file permissions
+            old_stat = File.stat(file_name)
+          else
+            # If not possible, probe which are the default permissions in the
+            # destination directory.
+            old_stat = probe_stat_in(File.dirname(file_name))
+          end
+
+          # Overwrite original file with temp file
+          FileUtils.mv(temp_file.path, file_name)
+
+          # Set correct permissions on new file
+          begin
+            File.chown(old_stat.uid, old_stat.gid, file_name)
+            # This operation will affect filesystem ACL's
+            File.chmod(old_stat.mode, file_name)
+          rescue Errno::EPERM
+            # Changing file ownership failed, moving on.
+          end
+        end
+
+        def probe_stat_in(dir)
+          basename = [
+            '.permissions_check',
+            Thread.current.object_id,
+            Process.pid,
+            rand(1000000)
+          ].join('.')
+
+          file_name = File.join(dir, basename)
+          FileUtils.touch(file_name)
+          File.stat(file_name)
+        ensure
+          FileUtils.rm_f(file_name) if file_name
+        end
+
+        def find_caches
+          Dir.glob(File.join(@root, '**/*.cache'))
+        end
+
+        def gc!
+          caches = find_caches
+
+          new_size = @max_size * 0.75
+          num_to_delete = caches.size - new_size
+          return unless num_to_delete > 0
+
+          caches.sort_by! { |path| -File.mtime(path).to_i }
+          FileUtils.remove(caches[0, num_to_delete], force: true)
+
+          @size = find_caches.size
+        end
     end
   end
 end
